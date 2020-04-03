df5fcf233f774cdbe74d92ef48074378319d7b73
ana <anamaria.panait10@yahoo.com>
2020-04-03 17:59:26 +0300

    Added some constraints to the drawing area.

--- gui/GraphGeneratorApp.py
+++ gui/GraphGeneratorApp.py
@@ -1,6 +1,7 @@
 import kivy
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.config import Config
 
 kivy.require('2.0.0')
 
@@ -10,7 +11,8 @@ class NodeWidget(Widget):
 
     def __init__(self, nr, pos):
         super().__init__()
-        self.pos = [pos[0] - 25, pos[1] - 27]
+        self.size = [50, 50]
+        self.pos = [pos[0] - self.size[0] / 2, pos[1] - self.size[1] / 2]
         self.nr = nr  # this is the id of the node
 
 class MainViewWidget(Widget):
@@ -18,12 +20,31 @@ class MainViewWidget(Widget):
         if self.ids.graph_canvas.collide_point(*touch.pos):
             global maxid
             maxid += 1
-            n = NodeWidget(maxid, touch.pos)
-            self.add_widget(n)
+            nx = touch.pos[0] - self.ids.graph_canvas.pos[0]
+            ny = touch.pos[1] - self.ids.graph_canvas.pos[1]
+
+            if nx < 30:
+                nx = 30
+
+            if ny < 30:
+                ny = 30
+
+            if nx > self.ids.graph_canvas.size[0] - 30:
+                nx = self.ids.graph_canvas.size[0] - 30
+
+            if ny > self.ids.graph_canvas.size[1] - 30:
+                ny = self.ids.graph_canvas.size[1] - 30
+
+            n = NodeWidget(maxid, [nx, ny])
+            self.ids.graph_canvas.add_widget(n)
+            return True
+        else:
+            return super().on_touch_down(touch)
 
 
 class GraphGeneratorApp(App):
     def build(self):
+        Config.set('input', 'mouse', 'mouse,multitouch_on_demand') # disable multi-touch emulation
         mainViewWidget = MainViewWidget()
 
         return mainViewWidget
